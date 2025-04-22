9a0d45c499f6308de48a7ce021669b3bf0fea0a2
hoangld <hoangld@onschool.edu.vn>
2025-04-22 22:47:36 +0700

    Model

---
+++ app/Services/OpenAi.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\ILLMModel;
+
+class OpenAi implements ILLMModel
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function ask($input): string
+    {
+
+    }
+}
